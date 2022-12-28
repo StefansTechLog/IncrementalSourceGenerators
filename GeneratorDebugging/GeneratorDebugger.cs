1043ecf1a23257b7c41289b0a2f9bad00aee96f0
StefansTechLog <socials@stefanstechlog.com>
2022-12-28 20:55:38 +0100

    Project Setup

---
+++ GeneratorDebugging/GeneratorDebugger.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace GeneratorDebugging
+{
+    internal static class GeneratorDebugger
+    {
+        internal static GeneratorDriverRunResult RunDebugging(
+            IEnumerable<SyntaxTree> sourceCode,
+            IIncrementalGenerator[] generators
+            )
+        {
+            var compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+
+            var inputCompilation = CSharpCompilation.Create("compilationAssemblyName", sourceCode, null, compilationOptions);
+
+            var driver = (GeneratorDriver)CSharpGeneratorDriver.Create(generators);
+
+            driver = driver.RunGeneratorsAndUpdateCompilation(inputCompilation, out _, out _);
+            return driver.GetRunResult();
+        }
+    }
+}
